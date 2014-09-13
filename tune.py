883db01d0504c617c220a9c9cc010c0bbac74cd2
Omar Awile <omar.awile@gmail.com>
2014-09-13 23:17:54 +0200

    Added proper signal handler for keyboard interrupt

--- tune.py
+++ tune.py
@@ -1,12 +1,14 @@
+import sys
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from Queue import Queue, Empty, Full
+import signal
 
 #                G      , C      , E      , A
 UKULELE_TONES = [391.995, 261.626, 329.628, 440.0]
-tone_names['G', 'C', 'E', 'A']
+tone_names = ['G', 'C', 'E', 'A']
 colors = ['green','red','yellow','blue']
 
 CHUNK = 4096
@@ -17,16 +19,17 @@ TIME_FRAME = 5.0
 
 q = Queue()
 
+def signal_handler(signal, frame):
+    sys.exit(0)
+
 
 def process(in_data, frame_count, time_info, status):
     global q 
-    try: 
-        q.put_nowait(in_data) 
-        return (in_data, pyaudio.paContinue)
-    except KeyboardInterrupt:
-        return (in_data, pyaudio.paAbort)
+    q.put_nowait(in_data) 
+    return (in_data, pyaudio.paContinue)
 
 
+signal.signal(signal.SIGINT, signal_handler)
 
 p = pyaudio.PyAudio()
 
@@ -39,13 +42,11 @@ stream = p.open(format=FORMAT,
         frames_per_buffer=CHUNK,
         stream_callback=process)
 
-print("* recording")
+print("* start")
 
 stream.start_stream()
 
 frames = []
-t = [0.0]
-ct = 0.0
 
 plt.ion()
 plt.show()
@@ -58,38 +59,32 @@ for tone, color in zip(UKULELE_TONES, colors):
     plt.axvline(tone, ymin=0, ymax=1000, color=color)
 plt.draw()
 
-try:
-    while stream.is_active():
-        try:
-            data = q.get_nowait()
-        except Empty as e:
-            time.sleep(0.002)
-            continue
+while stream.is_active():
+    try:
+        data = q.get_nowait()
+    except Empty as e:
+        time.sleep(0.002)
+        continue
 
-        decoded = np.fromstring(data, 'Float32') 
-        frames.append(decoded) 
-       
-        spectrum = np.fft.rfft(decoded)
-        spectrum = np.abs(spectrum)
-        maxs = np.max(spectrum) 
-        nln, = plt.plot(freq, spectrum,'k')
-        plt.xlim([100.0, 600.0])
-        plt.ylim([0.0, maxs*1.2])
-        plt.yticks([])
+    decoded = np.fromstring(data, 'Float32') 
+    #frames.append(decoded) 
+   
+    fdata = np.fft.rfft(decoded)
+    pspectrum = np.abs(fdata)**2
+    #maxs = np.max(pspectrum) 
+    nln, = plt.plot(freq, pspectrum,'k')
+    plt.xlim([100.0, 600.0])
+    plt.ylim([0.0, 400.0])
+    plt.yticks([])
+    plt.draw()
+    if ln is not None:
+        ln.remove()
         plt.draw()
-        if ln is not None:
-            ln.remove()
-            plt.draw()
-        ln = nln
-
-        ct += TIME_FRAME
-        t.append(ct)
-except KeyboardInterrupt:
-        pass
+    ln = nln
 
 
 plt.ioff()
-print("* done recording")
+print("* stop")
 
 stream.stop_stream()
 stream.close()
